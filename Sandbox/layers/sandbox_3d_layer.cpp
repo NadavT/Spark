31b3f30b9411784d55aeffa81a24547883edb042
Nadav Tur <nadavt100@gmail.com>
2020-12-27 23:38:37 +0200

    Refactor

--- Sandbox/layers/sandbox_3d_layer.cpp
+++ Sandbox/layers/sandbox_3d_layer.cpp
@@ -33,10 +33,9 @@ Sandbox3DLayer::Sandbox3DLayer()
     addDrawable(std::dynamic_pointer_cast<Spark::Drawable>(m_drawables[0]));
     setDirLight({m_dirLightDirection[0], m_dirLightDirection[1], m_dirLightDirection[2]},
                 {m_dirLightColor[0], m_dirLightColor[1], m_dirLightColor[2]});
-    m_pointLights.push_back(
-        Spark::createPointLight({0, 0, -2.0f}, {0, 1, 0},
-                                std::dynamic_pointer_cast<Spark::Drawable>(Spark::createSphere(
-                                    {0, 0, -2.0f}, {0.3f, 0.3f, 0.3f}, 36, 18, {0.3f, 0.3f, 0.3f}))));
+    std::shared_ptr<Spark::Drawable> sphere =
+        Spark::createSphere({0, 0, -2.0f}, {0.3f, 0.3f, 0.3f}, 36, 18, {0.3f, 0.3f, 0.3f});
+    m_pointLights.push_back(Spark::createPointLight({0, 0, -2.0f}, {0, 1, 0}, sphere));
     addPointLight(*(m_pointLights.back()));
     setSpotLight({m_spotLightColor[0], m_spotLightColor[1], m_spotLightColor[2]});
 }
